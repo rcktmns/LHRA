c176d6bdcda65a45ef2210f4f1fd08a4d44d0c22
rcktmns <izzypetsel@gmail.com>
2025-06-22 17:29:39 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function HomePage() {
               alt="Lionheart Riding Academy Logo"
               width={120}
               height={120}
-              className="mx-auto mb-6"
+              className="mx-auto mb-4"
             />
           
             <h1 className="font-papyrus text-l sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-white mb-4 drop-shadow-lg tracking-wider">
